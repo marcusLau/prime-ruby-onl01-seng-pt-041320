d728abb8fce98410b9fba84044779d9ed9fcca28
Marcus Lau <lau.marcus76@gmail.com>
2020-04-19 04:47:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,10 @@
 # Add  code here!
 
 def prime?(num)
-  prime = false
   root = Math.sqrt(num)
   for 2 in 0..root
     if num % 2 == 0 
       return false
     end
-  return prime
+  return true
 end
