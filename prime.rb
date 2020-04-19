deec1b307c07e3e36f9deb072fbebdf2cdbc9e16
Marcus Lau <lau.marcus76@gmail.com>
2020-04-19 04:54:18 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+
+def prime?(num)
+  if num <= 1
+    return false
+  end
+  root = Math.sqrt(num)
+  for i in 2..root
+    if num % i == 0 
+      return false
+    end
+  end
+  return true
+end
